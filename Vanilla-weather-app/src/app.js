947b180641bb0f0e998e69fa6afba4368665749d
Inzhu Saudambekova <inzhu.imm@gmail.com>
2022-10-25 11:40:12 +0200

    fixed the displayForecast function

--- Vanilla-weather-app/src/app.js
+++ Vanilla-weather-app/src/app.js
@@ -35,8 +35,8 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class ="row">`;
-  days.forEach(function (forecastDay, index) {
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
@@ -71,7 +71,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "690b4b858e00e718ee179495437e2a39";
+  let apiKey = "e947cb2640f1db92e6a19005bc43b435";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   console.log(apiUrl);
